75fe356a783e0ce1d1dec753866a7f5857bcbec6
Sanjanaaa <161009429+SanjanaVichare@users.noreply.github.com>
2025-06-17 22:52:21 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -19,7 +19,7 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "sveneterprise01@gmail.com"
+    MAIL_USERNAME = "sventerprise01@gmail.com"
     MAIL_PASSWORD = "webl yztc yvjo sipj"
 
     # Uploads
